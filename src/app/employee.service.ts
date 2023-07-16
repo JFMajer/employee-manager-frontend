2b06656d52103d91da87241e2754f76e725a3067
JFMajer <jakubmajer@live.com>
2023-07-16 11:03:36 +0200

    added functions to get, add, delete and update employee

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee }  from './employee';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,26 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+
+  // The getEmployees() method will make a GET request to the Spring Boot backend to retrieve a list of employees.
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
   }
+
+
+  // The addEmployee() method will make a POST request to the Spring Boot backend to add an employee.
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  // The updateEmployee() method will make a PUT request to the Spring Boot backend to update an employee.
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  // The deleteEmployee() method will make a DELETE request to the Spring Boot backend to delete an employee.
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+
 }
