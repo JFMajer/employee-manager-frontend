2a4cc937670aac0608bf1cd379d09f03614c3e0f
JFMajer <jakubmajer@live.com>
2023-07-20 12:04:43 +0200

    added dockerfile, fixed HttpClientModule error

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee }  from './employee';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
